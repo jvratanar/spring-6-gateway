b3c5bcec080b2fa32d5b2682b49c477755efcf11
Janez Franc Vratanar <jvratanar@gmail.com>
2023-11-20 12:53:10 +0100

    Section 36, Chapter 342.

---
+++ src/main/java/guru/springframework/spring6gateway/config/SpringSecConfig.java
@@ -0,0 +1,23 @@
+package guru.springframework.spring6gateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SpringSecConfig {
+    @Bean
+    public SecurityWebFilterChain securityFilterChain(ServerHttpSecurity http) {
+        http.csrf((csrf) -> csrf.disable())
+                .authorizeExchange(authorize -> authorize.anyExchange()
+                        .authenticated())
+                .oauth2ResourceServer(resourceServer ->
+                        resourceServer.jwt(Customizer.withDefaults()));
+        return http.build();
+    }
+}
